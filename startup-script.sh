d8b5a50f8952891babff4d4df4ea8d9c7e1857e3
lilliekhogg <lilliekhogg@gmail.com>
2018-02-06 22:59:01 +0000

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -16,6 +16,7 @@ secretKey=`curl -s -H "Metadata-Flavor: Google"  \
               "http://metadata.google.internal/computeMetadata/v1/instance/attributes/secret"`;
 serverip=`curl -s -H "Metadata-Flavor: Google"  \
                "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ip"`;
+vmNumber=`curl -s -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/vms"`;
 
 
 #run the client
